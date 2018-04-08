58ace718a3c9a2074462b6a649e013a28cdd015c
James Cox <j-cox03@hotmail.com>
2018-04-08 23:05:49 +0100

    Fixed bug with tailing empty WHERE clause

--- server/graphql-config/resolvers/notes-resolver.js
+++ server/graphql-config/resolvers/notes-resolver.js
@@ -3,6 +3,9 @@ const map = require("lodash/map")
 const postgresCommand = require("./../../../database/postgres-command")
 const buildWhereClause = require("./util/build-where-clause")
 
+const NOTE_SQL_QUERY = `SELECT * FROM notes`
+const TAG_SQL_QUERY = `SELECT * FROM tags`
+
 async function notesResolver(_, {id, title, tags, ignoreCase}){
   const requestedNotes = await notes(id, title, ignoreCase)
   return await Promise.all(
@@ -11,14 +14,15 @@ async function notesResolver(_, {id, title, tags, ignoreCase}){
 }
 
 async function notes(id, title, ignoreCase){
-  const command = `SELECT * FROM notes WHERE ${buildWhereClause({id, title}, {ignoreCase})};`;
+  const criteria = buildWhereClause({id, title}, {ignoreCase});
+  const command = criteria ? `${NOTE_SQL_QUERY} WHERE ${criteria}` : NOTE_SQL_QUERY;
   const {rows} = await postgresCommand(command)
   return rows
 }
 
 async function findTagsForNote(note){
   const {id} = note
-  const tags = await postgresCommand(`SELECT * FROM tags WHERE id = '${id}';`)
+  const tags = await postgresCommand(`${TAG_SQL_QUERY} WHERE id = '${id}'`)
   return set(note, "tags", tags.rows)
 }
 
