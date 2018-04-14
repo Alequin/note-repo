023d02c34bef9be4ee119ed354bc84fd6c2df78f
James Cox <j-cox03@hotmail.com>
2018-04-14 11:17:07 +0100

    Made update source in schema (no resolver yet)

--- server/graphql-config/types/index.js
+++ server/graphql-config/types/index.js
@@ -4,23 +4,30 @@ const sourceType = require("./source-type")
 
 const sourceInputType = require("./source-input-type")
 const tagsToUpdateInputType = require("./tags-to-update-input-type")
+const sourcesToUpdateInputType = require("./sources-to-update-input-type")
 
-module.exports = `
-  type Tag { ${tagType} }
+const schema = (
+  `type Tag { ${tagType} }
   type Source { ${sourceType} }
   type Note { ${noteType} }
 
   input sourceArguments { ${sourceInputType} }
   input tagsToUpdateArguments { ${tagsToUpdateInputType} }
+  input sourcesToUpdateArguments { ${sourcesToUpdateInputType} }
 
   type Query {
     notes(id: Int title: String tags: [String] ignoreCase: Boolean): [Note!]!
     tags: [Tag!]!
   }
-  
+
   type Mutation {
     insertTags(names: [String!]!): [Tag]
     updateTags(tags: [tagsToUpdateArguments!]!): [Tag]
     insertSources(sources: [sourceArguments!]!): [Source]
-  }
-`
+    updateSources(sources: [sourcesToUpdateArguments!]!): [Source]
+  }`
+)
+
+console.log(schema);
+
+module.exports = schema
---
+++ server/graphql-config/types/sources-to-update-input-type.js
@@ -0,0 +1,12 @@
+const {sourcesSchema} = require("./../../../database/schema")
+
+const {
+  columns: {
+    name
+  }
+} = sourcesSchema
+
+module.exports = `
+  old${name.name}: String!
+  update: sourceArguments!
+`
