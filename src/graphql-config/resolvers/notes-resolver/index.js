dbe8e010692989bdac4e7e763be2b6e5efa984f6
James Cox <j-cox03@hotmail.com>
2018-05-05 21:48:50 +0100

    added empty list condition to notes resolver

--- src/graphql-config/resolvers/notes-resolver/index.js
+++ src/graphql-config/resolvers/notes-resolver/index.js
@@ -2,7 +2,7 @@ const findNotes = require("./find-notes")
 const findNotesWithTags = require("./find-notes-with-tags")
 
 async function notesResolver(_parent, {id, title, tags, ignoreCase}){
-  if(tags){
+  if(tags && tags.length > 0){
     return await findNotesWithTags(id, title, tags, ignoreCase)
   }else{
     return await findNotes(id, title, ignoreCase)
